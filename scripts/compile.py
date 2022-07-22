ebe8445c3a967dfebd6736b99bbc31c3ca63b05b
ecyht2 <ecyht2@nottingham.edu.my>
2022-07-22 13:34:30 +0800

    Added ejs compilation script

---
+++ scripts/compile.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+from pathlib import Path, PosixPath, WindowsPath
+from os.path import join, dirname
+from os import PathLike
+
+
+def get_html(directory: PosixPath | WindowsPath) -> list[PosixPath |
+                                                         WindowsPath]:
+    # Getting all the html files
+    html_files = list(directory.glob('*.html'))
+
+    # Looping into subdirectories
+    for i in directory.iterdir():
+        if i.is_dir():
+            for j in get_html(i):
+                html_files.append(j)
+
+    return html_files
+
+
+def write_ejs(html_file: PathLike, repository: PathLike) -> None:
+    html_file = Path(html_file)
+    # Reading and editing html file
+    edited = ""
+    with open(html_file, 'r') as f:
+        for line in f:
+            match line.strip():
+                case "<script src=\"scripts/render_ejs.js\"></script>":
+                    # Deleting Lines
+                    line = ""
+                case "<script src=\"scripts/ejs.min.js\"></script>":
+                    # Deleting Lines
+                    line = ""
+                case "<base href=\"../../\">":
+                    # Deleting Lines
+                    line = ""
+                # https://stackoverflow.com/questions/2308944/multiple-value-checks-using-in-operator-python
+                case _ if any(string in line for string in ("href", "src")):
+                    # Links
+                    # Getting Indexes
+                    if "href" in line:
+                        index = line.find("href")
+                        index_left = index + len("href") + 2
+                        index_right = line.find("\"", index_left)
+                    else:
+                        index = line.find("src")
+                        index_left = index + len("src") + 2
+                        index_right = line.find("\"", index_left)
+
+                    # Getting and Formatting Link
+                    link = line[index_left:index_right]
+                    partition = list(line.partition(link))
+                    if link.startswith(("/", "http")):
+                        link = link
+                    else:
+                        link = "/" + link
+                    partition[1] = link
+
+                    line = "".join(partition)
+            edited += line
+
+    # Writing ejs files
+    parent = Path(dirname(html_file)).name
+    results_dir = join(repository, 'views', parent)
+    results_path = join(results_dir, html_file.name.replace('html', 'ejs'))
+    with open(results_path, 'w') as f:
+        f.write(edited)
+
+
+p = Path(__file__)
+repository = p.parent.parent
+html_dir = Path(join(repository, 'html'))
+html_files = get_html(html_dir)
+for html_file in html_files:
+    write_ejs(html_file, repository)
