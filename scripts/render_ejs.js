b0ec07b9dc3684b01a8c96cc3c1e861519af5a3c
ecyht2 <ecyht2@nottingham.edu.my>
2022-07-24 10:34:01 +0800

    Updated client ejs rendering script

--- scripts/render_ejs.js
+++ scripts/render_ejs.js
@@ -1,6 +1,13 @@
 // https://teddit.net/r/learnjavascript/comments/m1tyv5/how_to_runopen_an_ejs_file_on_browser/
+// https://web.dev/read-files/
+// https://stackoverflow.com/questions/371875/local-file-access-with-javascript
 const bodyHTML = document.body
   .innerHTML
   .replace(/&lt;/g, '<')
   .replace(/&gt;/g, '>');
-document.body.innerHTML = ejs.render(bodyHTML);
+const fn = ejs.compile(bodyHTML, { client: true });
+
+document.body.innerHTML = fn(null, null, function(path, data) {
+  console.log(path);
+  console.log(data);
+});
